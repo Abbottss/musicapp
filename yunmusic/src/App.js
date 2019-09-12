8c690492a706f84e06059c99eb88437158aedfe5
tianhaottt <18435995792@163.com>
2019-09-12 18:43:17 +0800

    基本架构

--- yunmusic/src/App.js
+++ yunmusic/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import My from "./views/My";
+import Discover from "./views/Discover";
+import YunVillage from "./views/YunVillage";
+import Video from "./views/Video";
+import Search from "./views/Search";
 
+
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink,
+    Switch
+} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <nav>
+                <NavLink to={"/my"} >我的</NavLink>
+                <NavLink to={"/discover"} >发现</NavLink>
+                <NavLink to={"/yunvillage"} >云村</NavLink>
+                <NavLink to={"/video"}>视频</NavLink>
+                <NavLink to={"/search"}>搜索</NavLink>
+            </nav>
+            <Route path={"/my"} component={My}></Route>
+            <Route path={"/discover"} component={Discover}></Route>
+            <Route path={"/yunvillage"} component={YunVillage}></Route>
+            <Route path={"/video"} component={Video}></Route>
+            <Route path={"/search"} component={Search}></Route>
+        </Router>
     </div>
   );
 }
