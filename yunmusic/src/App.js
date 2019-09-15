32187a0063c908bf1659d973d22f5ec78255ff84
tianhaottt <18435995792@163.com>
2019-09-15 18:23:53 +0800

    抽屉

--- yunmusic/src/App.js
+++ yunmusic/src/App.js
@@ -6,33 +6,43 @@ import Discover from "./views/Discover";
 import YunVillage from "./views/YunVillage";
 import Video from "./views/Video";
 import Search from "./views/Search";
-
-
+import DrawerLeft from "./components/Drawer"
 import {
     BrowserRouter as Router,
     Route,
     NavLink,
     Switch
 } from "react-router-dom"
-function App() {
-  return (
-    <div className="App">
-        <Router>
-            <nav>
-                <NavLink to={"/my"} >我的</NavLink>
-                <NavLink to={"/discover"} >发现</NavLink>
-                <NavLink to={"/yunvillage"} >云村</NavLink>
-                <NavLink to={"/video"}>视频</NavLink>
-                <NavLink to={"/search"}>搜索</NavLink>
-            </nav>
-            <Route path={"/my"} component={My}></Route>
-            <Route path={"/discover"} component={Discover}></Route>
-            <Route path={"/yunvillage"} component={YunVillage}></Route>
-            <Route path={"/video"} component={Video}></Route>
-            <Route path={"/search"} component={Search}></Route>
-        </Router>
-    </div>
-  );
+class App extends React.Component{
+    render(){
+        return (
+            <div >
+                <DrawerLeft></DrawerLeft>
+                <Router>
+                    <nav>
+                        <NavLink to={"/my"} >我的</NavLink>
+                        <NavLink to={"/discover"} >发现</NavLink>
+                        <NavLink to={"/yunvillage"} >云村</NavLink>
+                        <NavLink to={"/video"}>视频</NavLink>
+                        <NavLink to={"/search"}>搜索</NavLink>
+                    </nav>
+                    <Switch>
+                        <Route path={"/my"} component={My}></Route>
+                        <Route path={"/discover"} component={Discover}></Route>
+                        <Route path={"/yunvillage"} component={YunVillage}></Route>
+                        <Route path={"/video"} component={Video}></Route>
+                        <Route path={"/search"} component={Search}></Route>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+  // return (
+  //   <div className="App">
+
+  //   </div>
+  // );
+
 }
 
 export default App;
