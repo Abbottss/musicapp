14a17340d651376fda881a0c1095f3cdb4ec4f78
SunshineMyto <454481545@qq.com>
2019-09-17 19:27:38 +0800

    登录的页面未成版

--- yunmusic/src/App.js
+++ yunmusic/src/App.js
@@ -1,82 +1,25 @@
-import React from 'react';
+import React,{Component} from "react";
 import logo from './logo.svg';
 import './App.css';
-import './aseets/style/background.css';
-import My from "./views/My";                    //我的
-import Discover from "./views/Discover";        //发现
-import YunVillage from "./views/YunVillage";    //云村
-import Video from "./views/Video";              //视频
-import Search from "./views/Search";            //搜索
-import {Button,Layout} from 'antd';
-import { Row, Col } from 'antd';
-import DrawerLeft from "./components/Drawer"    //个人信息展开栏（抽屉）
-import Carousel from "./views/Discover/Carousel"
+import Common from "./views/Common";
+import Login from "./views/Login";
+import Cellphone from "./views/Cellphone";
 import {
-    BrowserRouter as Router,
-    Route,
-    NavLink,
-    Switch,
-    Redirect
-} from "react-router-dom"
-const { Header, Footer, Sider, Content } = Layout;
-
-class App extends React.Component{
-    render(){
-        return (
-
-            <div >
-                <Router>
-                    <Layout> 
-                        <Header>
-                            <Row type="flex" justify="space-between">
-                                <nav className="App-header">
-                            
-                                    <Col span={4}><DrawerLeft></DrawerLeft></Col>
-                                    <Col span={4}><NavLink to={"/my"} activeStyle={{color:'#fff',fontSize:'16px'}}>我的</NavLink></Col>
-                                    <Col span={4}><NavLink to={"/discover"} activeStyle={{color:'#fff',fontSize:'16px'}}>发现</NavLink></Col>
-                                    <Col span={4}><NavLink to={"/yunvillage"} activeStyle={{color:'#fff',fontSize:'16px'}}>云村</NavLink></Col>
-                                    <Col span={4}><NavLink to={"/video"} activeStyle={{color:'#fff',fontSize:'16px'}}>视频</NavLink></Col>
-                                    <Col span={4}><NavLink to={"/search"} activeStyle={{color:'#fff',fontSize:'16px'}}>搜索</NavLink></Col>
-                                    
-                                </nav>
-                            </Row>
-                        </Header>
-                    </Layout>
-                        <Switch>
-                                    
-                            <Route path={"/my"} component={My}></Route>
-                            <Route path={"/discover"} component={Discover}></Route>
-                            <Route path={"/yunvillage"} component={YunVillage}></Route>
-                            <Route path={"/video"} component={Video}></Route>
-                            <Route path={"/search"} component={Search}></Route>
-                            <Redirect from="/" to="/discover" />
-                        </Switch>
-                </Router> 
-                       
-                   
-                    {/* <Content>
-                        <Carousel></Carousel> 
-                        1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>
-                        1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>1<br/>
-                    </Content>
-                   
-                    <Footer>
-                        111
-                    </Footer> */}
-                
-                
-                
-            </div>
-            // mountNode,
-            
-        );
-    }
-  // return (
-  //   <div className="App">
-
-  //   </div>
-  // );
-
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from "react-router-dom";
+class App extends Component{
+	render(){
+		return (
+			<Router>
+				<Switch>
+					<Route path={"/login"} component={Login}></Route>
+					<Route path={"/cellphone"} component={Cellphone}></Route>	
+					<Route path={"/"} component={Common}></Route>
+				</Switch>
+			</Router>
+		)
+	}
 }
-
 export default App;
---
+++ yunmusic/src/views/Cellphone.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Cellphone extends React.Component{
+	render(){
+		return (
+			<div>手机端登录</div>
+		)
+	}
+}
+export default Cellphone;
